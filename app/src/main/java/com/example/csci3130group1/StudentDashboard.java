a818a80c914493cb42779a3c0a20bf0132f611b9
grainnie <gv749789@dal.ca>
2025-03-11 16:31:43 -0300

    Working on dynamic dashboard

--- app/src/main/java/com/example/csci3130group1/StudentDashboard.java
+++ app/src/main/java/com/example/csci3130group1/StudentDashboard.java
@@ -12,24 +12,29 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.csci3130group1.databinding.ActivityTutorDashboardBinding;
+import com.example.csci3130group1.databinding.ActivityStudentDashboardBinding;
 
-public class TutorDashboard extends AppCompatActivity {
-    private ActivityTutorDashboardBinding binding;
+public class StudentDashboard extends AppCompatActivity {
+
+    private ActivityStudentDashboardBinding binding;
+
     private TextView welcomeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityTutorDashboardBinding.inflate(getLayoutInflater());
+
+        binding = ActivityStudentDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_profile, R.id.navigation_tutorial_management, R.id.navigation_recommendations)
+                R.id.navigation_profile, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_tutor_dashboard);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_student_dashboard);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
         welcomeText = findViewById(R.id.welcome_text);
 // NEW: Get username, role, and password from intent
