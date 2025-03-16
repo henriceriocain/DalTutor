b6a0d6237e1c1792fafdc089181aa558a524aae0
grainnie <gv749789@dal.ca>
2025-03-16 15:58:27 -0300

    Implementing US7 current location with US8

--- app/src/main/java/com/example/csci3130group1/ui/search_for_tutorials/SearchForTutorialsFragment.java
+++ app/src/main/java/com/example/csci3130group1/ui/search_for_tutorials/SearchForTutorialsFragment.java
@@ -1,33 +1,54 @@
 
 package com.example.csci3130group1.ui.search_for_tutorials;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import com.google.android.gms.location.LocationRequest;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.csci3130group1.R;
 import com.example.csci3130group1.databinding.FragmentSearchForTutorialsBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class SearchForTutorialsFragment extends Fragment {
-
+    Button btLocation;
+    TextView location_text;
+    FusedLocationProviderClient client;
     private FragmentSearchForTutorialsBinding binding;
     private ListView tutorialListView;
     private List<Tutorial> tutorialResults;
@@ -37,6 +58,24 @@ public class SearchForTutorialsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSearchForTutorialsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        btLocation = root.findViewById(R.id.bt_location);
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        btLocation.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&  ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                            getCurrentLocation();
+                        }
+                        else {
+                            requestPermissions(
+                                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                                    100);
+                        }
+                    }
+                }
+        );
+
 
         // Initialize UI elements
         tutorialListView = binding.tutorialListView;
@@ -74,6 +113,66 @@ public class SearchForTutorialsFragment extends Fragment {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && (grantResults.length > 0) && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+            getCurrentLocation();
+        }
+        else {
+            Toast.makeText(getActivity(),"Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        Geocoder geocoder = new Geocoder(this.getContext(), Locale.getDefault());
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            client.getLastLocation().addOnCompleteListener(
+                    new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            Location location = task.getResult();
+                            if (location != null) {
+                                try {
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    String cityName = addresses.get(0).getAddressLine(0);
+                                    View root = binding.getRoot();
+                                    location_text = root.findViewById(R.id.location_text);
+                                    location_text.setText(cityName);
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            else {
+                                LocationRequest locationRequest = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(1000).setFastestInterval(1000).setNumUpdates(1);
+                                LocationCallback locationCallback = new LocationCallback() {
+                                    @Override
+                                    public void onLocationResult(LocationResult locationResult) {
+                                        Location location1 = locationResult.getLastLocation();
+                                        try {
+                                            List<Address> addresses = geocoder.getFromLocation(location1.getLatitude(), location1.getLongitude(), 1);
+                                            String cityName = addresses.get(0).getAddressLine(0);
+                                            View root = binding.getRoot();
+                                            location_text = root.findViewById(R.id.location_text);
+                                            location_text.setText(cityName);
+                                        } catch (IOException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    }
+                                };
+                                client.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                            }
+                        }
+                    }
+            );
+        }
+        else {
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
 
 
     @Override
