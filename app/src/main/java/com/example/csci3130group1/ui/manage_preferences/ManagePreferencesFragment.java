89ebda09627e7c04d0e6e43759325dbc33aa73d6
grainnie <gv749789@dal.ca>
2025-03-26 21:23:56 -0300

    Spinner of tutors exists in manage preferences for Student but isnt interactable

--- app/src/main/java/com/example/csci3130group1/ui/manage_preferences/ManagePreferencesFragment.java
+++ app/src/main/java/com/example/csci3130group1/ui/manage_preferences/ManagePreferencesFragment.java
@@ -4,25 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.csci3130group1.R;
 import com.example.csci3130group1.databinding.FragmentManagePreferencesBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 
 public class ManagePreferencesFragment extends Fragment {
 
     private FragmentManagePreferencesBinding binding;
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference usersdRef = rootRef.child("users");
+    ArrayList<String> tutorNames = new ArrayList<>();
+    ValueEventListener eventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            tutorNames.add("Select a Tutor");
+            for(DataSnapshot ds : snapshot.getChildren()) {
+                if (Objects.equals(ds.child("role").getValue(String.class), "Tutor")) {
+                    String name = ds.child("name").getValue(String.class);
+                    tutorNames.add(name);
+                }
+            }
+        }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ManagePreferencesViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(ManagePreferencesViewModel.class);
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {}
+    };
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        usersdRef.addValueEventListener(eventListener);
         binding = FragmentManagePreferencesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        setTutorSpinner(root);
         return root;
     }
 
@@ -30,5 +58,12 @@ public class ManagePreferencesFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void setTutorSpinner(View view) {
+        Spinner spinner = view.findViewById(R.id.tutor);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, tutorNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
