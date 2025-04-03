0e9c4b00ac657cd9c9e0805779aed8308a937a97
grainnie <gv749789@dal.ca>
2025-04-03 18:00:48 -0300

    Small changes to code

--- app/src/androidTest/java/com/example/csci3130group1/US11_Espresso.java
+++ app/src/androidTest/java/com/example/csci3130group1/US11_Espresso.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class US11_espresso {
+public class US11_Espresso {
 
     @Rule
     public ActivityScenarioRule<LoginActivity> activityRule = new ActivityScenarioRule<>(LoginActivity.class);
