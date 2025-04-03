e6b179b765d14bb7ca042d69ba2b0bfcce88d885
rishabhs <rs907830@dal.ca>
2025-04-02 21:08:44 -0300

    Failing espresso test case for US11

--- app/src/androidTest/java/com/example/csci3130group1/US11_espresso.java
+++ app/src/androidTest/java/com/example/csci3130group1/US11_espresso.java
@@ -13,6 +13,9 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
+import android.os.SystemClock;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -20,46 +23,37 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-
 @RunWith(AndroidJUnit4.class)
 public class US11_espresso {
 
     @Rule
-    public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule<>(LoginActivity.class);
+    public ActivityScenarioRule<LoginActivity> activityRule = new ActivityScenarioRule<>(LoginActivity.class);
 
     @Test
     public void testManagePreferencesEspresso() {
 
-        onView(withId(R.id.username_input))
-                .perform(typeText("gv749789@dal.ca"));
+        onView(withId(R.id.username_input)).perform(typeText("gv749789@dal.ca"));
         closeSoftKeyboard();
 
-
-        onView(withId(R.id.password_input))
-                .perform(typeText("Gavin26672!"));
+        onView(withId(R.id.password_input)).perform(typeText("Gavin26672!"));
         closeSoftKeyboard();
-
 
         onView(withId(R.id.role_spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("Student"))).perform(click());
         onView(withId(R.id.role_spinner)).check(matches(withSpinnerText("Student")));
 
-
         onView(withId(R.id.button2)).perform(click());
 
+        SystemClock.sleep(3000);
 
         onView(withText("Manage Preferences")).perform(click());
 
-
         onView(withId(R.id.CS)).perform(click());
-
 
         onView(withId(R.id.tutor)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("Gavin Rainnie"))).perform(click());
         onView(withId(R.id.tutor)).check(matches(withSpinnerText("Gavin Rainnie")));
 
-
         onView(withId(R.id.savepref)).perform(click());
-
     }
 }
